023ef7a9dad024f2c59830e4749b55561e2d0c5b
Nicky Ye <niye@itu.dk>
2024-09-17 11:04:08 +0200

    done?

--- Ass2/tcp.go
+++ Ass2/tcp.go
@@ -1,9 +1,14 @@
 package main
 
-type TCPPackage struct{
+import (
+	"fmt"
+	"time"
+)
+
+type TCPPackage struct {
 	message string
-	ack int
-	seq int
+	seq     int
+	ack     int
 }
 
 func main() {
@@ -11,12 +16,49 @@ func main() {
 
 	go client(channel)
 	go server(channel)
+	fmt.Println("Client and server spawned")
+	time.Sleep(3 * time.Second)
 }
 
-func client(chan cqwe){
+func client(channel chan TCPPackage) {
+	message := TCPPackage{"request", 0, 0}
+	channel <- message
+	fmt.Printf("Client sent request package with sequence: %d\n", message.seq)
+
+	response := <-channel
+	fmt.Printf("Client pulled out again with sequence : %d\n", response.seq)
+	if response.seq == 0 {
+		channel <- response
+		fmt.Printf("Client sent back with sequence: %d\n", response.seq)
+	} else {
+		fmt.Printf("Client retrieved response with sequence: %d\n", response.seq)
+		fmt.Printf("Client retrieved response with ack: %d\n", response.ack)
+		response.seq += 1
+		response.ack += 1
+		channel <- response
+	}
 
 }
 
-func server(chan wqe){
+func server(channel chan TCPPackage) {
+	message := <-channel
+	fmt.Printf("Server retrieved package with sequence: %d\n", message.seq)
+	message.seq += 1
+	message.ack = 1
+
+	channel <- message
+	fmt.Printf("Server sent back response with seq: %d and ack: %d\n", message.seq, message.ack)
+
+	response := <-channel
+	if response.seq == 1 {
+		channel <- response
+	} else {
+		fmt.Printf("Server retrieved with sequence: %d, ack: %d and message: %s\n", response.seq, response.ack, response.message)
+	}
 
 }
+
+/*
+1)[Easy] Implement the TCP/IP Handshake using threads. This is not realistic (since the protocol should run across a network)
+but your implementation needs to show that you have a good understanding of the protocol.
+*/

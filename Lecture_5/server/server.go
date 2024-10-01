91d042316d680aadd368fccc15323b34ecd2b88f
Nicky Ye <niye@itu.dk>
2024-10-01 15:22:05 +0200

    fix return time

--- Lecture_5/server/server.go
+++ Lecture_5/server/server.go
@@ -1,9 +1,21 @@
 package main
 
-import(
-	pb "Lecture_5"
+import (
+	pb "Lecture_5/proto"
+	"context"
+	"time"
 )
 
-func main(){
+type server struct {
+	pb.UnimplementedTimeServiceServer
+}
+
+func (s *server) GetTime(ctx context.Context, in *pb.Empty) (*pb.Time, error) {
+	tid := time.Now()
+	t := tid.String()
+	return &pb.Time{Time: t}, nil
+}
+
+func main() {
 
 }

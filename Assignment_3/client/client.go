3b24feb7fd5bf33bbad5c4b9b51bbbd0922c3a88
Nicky Ye <niye@itu.dk>
2024-10-16 21:50:19 +0200

    working bad chat missing timestamp

--- Assignment_3/client/client.go
+++ Assignment_3/client/client.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	proto "Assignment_3/proto"
+	"bufio"
 	"context"
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -18,6 +21,52 @@ func main() {
 
 	client := proto.NewChittyChatServiceClient(conn)
 
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("enter command\n join to join a session\nquit to quit program\n")
+		command, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatalf("failed to read")
+		}
+
+		command = strings.TrimSpace(command)
+		if command == "join" {
+			fmt.Println("joined")
+			for {
+				fmt.Print("enter command\n leave to leave\n send to send message\n get to get messages\n")
+				todo, erro := reader.ReadString('\n')
+				todo = strings.TrimSpace(todo)
+				if erro != nil {
+					log.Fatalf("failed to read")
+				}
+
+				if todo == "leave" {
+					fmt.Println("left")
+					break
+				}
+
+				if todo == "send" {
+					fmt.Println("enter message")
+					tosend, erro := reader.ReadString('\n')
+					if erro != nil {
+						log.Fatalf("failed to read")
+					}
+					tosend = strings.TrimSpace(tosend)
+					sendMessage(client, tosend)
+					getMessages(client)
+				}
+
+				if todo == "get" {
+					getMessages(client)
+				}
+			}
+		}
+		if command == "quit" {
+			break
+		}
+
+	}
+	fmt.Println("done")
 	// setup cli with send message command, get messages command, join command, quit command,
 	// send messages takes 1 arg the messages to send
 	// get messages takes 0 args
@@ -30,18 +79,24 @@ func main() {
 	// pass message and timstamp into sendmessage remote function
 	// to compute nodenr when client joining send join request, server will compute an id for client, when new client joins compute another id and send back
 
-	message, errr := client.SendMessage(context.Background(), &proto.Message{Message: "hej", Timestamp: 1})
-	if errr != nil {
+	// this is not live chatting
+	// can use bi directional streaming*
+
+}
+
+func sendMessage(client proto.ChittyChatServiceClient, messag string) {
+	message, err := client.SendMessage(context.Background(), &proto.Message{Message: messag, Timestamp: 1})
+	if err != nil {
 		log.Fatal(err.Error())
 	}
 	log.Printf("Sent message: %s, with timestamp: %d\n", message.Message, message.Timestamp)
+}
 
+func getMessages(client proto.ChittyChatServiceClient) {
 	messages, err := client.GetMessages(context.Background(), &proto.Empty{})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	for _, message := range messages.Messages {
-		fmt.Printf(" - %s, : %d \n", message.Message, message.Timestamp)
-	}
+	fmt.Println(messages.Messages[len(messages.Messages)-1].Message)
 }
